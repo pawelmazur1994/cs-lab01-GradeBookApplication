24d4aa68f2976ea0c9cb6505efa54f1ac323d080
pawelmazur1994 <66783018+pawelmazur1994@users.noreply.github.com>
2022-04-03 16:49:31 +0200

    Override GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,15 +2,29 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-
         public RankedGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+            else if (averageGrade >= 80) 
+                return 'A';
+            else if (averageGrade >= 60 && averageGrade < 80) 
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade < 60) 
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade < 40) 
+                return 'D';
+            else return 'F';
+
+        }
+
     }
+
 }
